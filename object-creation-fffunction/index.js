16130ab7972bcb73f7ab22fd2e0d4dd22daad310
cfsanderson <calebsanderson@gmail.com>
2017-09-01 15:40:29 -0400

    Changes object-creation-fffunction file names for organization.

--- object-creation-fffunction/index.js
+++ object-creation-fffunction/index.js
@@ -1,15 +1,15 @@
-// let name
-//
-// const getYourName = () => {
-//   name = prompt("What is your name?");
-// }
-// getYourName()
-//
-// const printYourName = () => {
-//   if(name){
-//     document.getElementById("sayHello").innerHTML = "Hello " + name
-//   } else {
-//     alert("Please Enter Your Name Next Time");
-//   }
-// }
-// printYourName()
+let name
+
+const getYourName = () => {
+  name = prompt("What is your name?");
+}
+getYourName()
+
+const printYourName = () => {
+  if(name){
+    document.getElementById("sayHello").innerHTML = "Hello " + name
+  } else {
+    alert("Please Enter Your Name Next Time");
+  }
+}
+printYourName()
