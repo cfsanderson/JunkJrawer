467b7541693f92805f9e2e70f6335113707a1c16
cfsanderson <calebsanderson@gmail.com>
2017-11-27 21:44:30 -0500

    Adds eslint-config-prettier and updates .eslintrc

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,15 +1,15 @@
-let name
+let name;
 
 const getYourName = () => {
   name = prompt("What is your name?");
-}
-getYourName()
+};
+getYourName();
 
 const printYourName = () => {
-  if(name){
-    document.getElementById("sayHello").innerHTML = "Hello " + name + " :)"
+  if (name) {
+    document.getElementById("sayHello").innerHTML = `Hello ${  name  } :)`;
   } else {
     alert("Please Enter Your Name Next Time");
   }
-}
-printYourName()
+};
+printYourName();
